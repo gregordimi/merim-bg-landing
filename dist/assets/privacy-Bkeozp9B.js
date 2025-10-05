be739ebafec5d6a431ff131f138977ac6056ddee
Gigi <grdimitrov@gmail.com>
2025-10-05 22:34:12 +0200

    include /dist folder

---
+++ dist/assets/privacy-Bkeozp9B.js
@@ -0,0 +1,55 @@
+import{u as i,j as n}from"./blog-chunk-0v7l2AT3.js";import"./react-vendor-DcQ1tznY.js";const d=void 0;function l(r){const s={a:"a",br:"br",em:"em",h1:"h1",h2:"h2",hr:"hr",li:"li",ol:"ol",p:"p",strong:"strong",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",ul:"ul",...i(),...r.components};return n.jsxs(n.Fragment,{children:[n.jsx(s.h1,{children:"Политика за поверителност"}),`
+`,n.jsx(s.p,{children:"Последна актуализация: 1 януари 2025 г."}),`
+`,n.jsx(s.h2,{children:"Събиране на информация"}),`
+`,n.jsx(s.p,{children:"Ние събираме минимално количество данни, необходими за функционирането на приложението:"}),`
+`,n.jsxs(s.ul,{children:[`
+`,n.jsxs(s.li,{children:[n.jsx(s.strong,{children:"Данни за използване"}),": Информация за това как използвате приложението"]}),`
+`,n.jsxs(s.li,{children:[n.jsx(s.strong,{children:"Технически данни"}),": IP адрес, тип браузър, операционна система"]}),`
+`,n.jsxs(s.li,{children:[n.jsx(s.strong,{children:"Данни за цени"}),": Цени, които споделяте в приложението"]}),`
+`]}),`
+`,n.jsx(s.h2,{children:"Използване на данните"}),`
+`,n.jsx(s.p,{children:"Данните се използват за:"}),`
+`,n.jsxs(s.ol,{children:[`
+`,n.jsxs(s.li,{children:[n.jsx(s.strong,{children:"Подобряване на услугите"})," - Анализ на поведението на потребителите"]}),`
+`,n.jsxs(s.li,{children:[n.jsx(s.strong,{children:"Предоставяне на точна информация"})," - Обработка и показване на цени"]}),`
+`,n.jsxs(s.li,{children:[n.jsx(s.strong,{children:"Техническа поддръжка"})," - Решаване на проблеми и оптимизация"]}),`
+`]}),`
+`,n.jsx(s.h2,{children:"Споделяне на данни"}),`
+`,n.jsxs(s.p,{children:["Ние ",n.jsx(s.strong,{children:"НЕ"})," споделяме вашите лични данни с трети страни, освен в следните случаи:"]}),`
+`,n.jsxs(s.ul,{children:[`
+`,n.jsx(s.li,{children:"При законово изискване"}),`
+`,n.jsx(s.li,{children:"За защита на нашите права"}),`
+`,n.jsx(s.li,{children:"С ваше изрично съгласие"}),`
+`]}),`
+`,n.jsx(s.h2,{children:"Защита на данните"}),`
+`,n.jsx(s.p,{children:"Ние използваме съвременни технологии за защита:"}),`
+`,n.jsxs(s.table,{children:[n.jsx(s.thead,{children:n.jsxs(s.tr,{children:[n.jsx(s.th,{children:"Мярка"}),n.jsx(s.th,{children:"Описание"})]})}),n.jsxs(s.tbody,{children:[n.jsxs(s.tr,{children:[n.jsx(s.td,{children:"Криптиране"}),n.jsx(s.td,{children:"SSL/TLS криптиране на данните"})]}),n.jsxs(s.tr,{children:[n.jsx(s.td,{children:"Достъп"}),n.jsx(s.td,{children:"Ограничен достъп до данните"})]}),n.jsxs(s.tr,{children:[n.jsx(s.td,{children:"Мониторинг"}),n.jsx(s.td,{children:"Постоянно наблюдение за сигурност"})]})]})]}),`
+`,n.jsx(s.h2,{children:"Вашите права"}),`
+`,n.jsx(s.p,{children:"Имате право да:"}),`
+`,n.jsxs(s.ul,{children:[`
+`,n.jsxs(s.li,{children:[n.jsx(s.strong,{children:"Достъп"}),": Поискате копие от вашите данни"]}),`
+`,n.jsxs(s.li,{children:[n.jsx(s.strong,{children:"Корекция"}),": Поправите неточни данни"]}),`
+`,n.jsxs(s.li,{children:[n.jsx(s.strong,{children:"Изтриване"}),": Поискате изтриване на данните"]}),`
+`,n.jsxs(s.li,{children:[n.jsx(s.strong,{children:"Ограничаване"}),": Ограничите обработката"]}),`
+`]}),`
+`,n.jsx(s.h2,{children:"Съхранение на данни"}),`
+`,n.jsx(s.p,{children:"Данните се съхраняват:"}),`
+`,n.jsxs(s.ul,{children:[`
+`,n.jsxs(s.li,{children:[n.jsx(s.strong,{children:"Данни за използване"}),": 2 години"]}),`
+`,n.jsxs(s.li,{children:[n.jsx(s.strong,{children:"Технически данни"}),": 1 година"]}),`
+`,n.jsxs(s.li,{children:[n.jsx(s.strong,{children:"Данни за цени"}),": Безсрочно (анонимизирани)"]}),`
+`]}),`
+`,n.jsx(s.h2,{children:"Промени в политиката"}),`
+`,n.jsx(s.p,{children:"Запазваме си правото да променяме тази политика. При промени ще ви уведомим чрез:"}),`
+`,n.jsxs(s.ul,{children:[`
+`,n.jsx(s.li,{children:"Известие в приложението"}),`
+`,n.jsx(s.li,{children:"Имейл (ако сте предоставили такъв)"}),`
+`,n.jsx(s.li,{children:"Публикуване на уебсайта"}),`
+`]}),`
+`,n.jsx(s.h2,{children:"Контакт"}),`
+`,n.jsx(s.p,{children:"За въпроси относно тази политика:"}),`
+`,n.jsxs(s.p,{children:[n.jsx(s.strong,{children:"Имейл"}),": ",n.jsx(s.a,{href:"mailto:privacy@merim.bg",children:"privacy@merim.bg"}),n.jsx(s.br,{}),`
+`,n.jsx(s.strong,{children:"Адрес"}),": София, България",n.jsx(s.br,{}),`
+`,n.jsx(s.strong,{children:"Телефон"}),": +359 XXX XXX XXX"]}),`
+`,n.jsx(s.hr,{}),`
+`,n.jsx(s.p,{children:n.jsx(s.em,{children:"Вашата поверителност е важна за нас!"})})]})}function c(r={}){const{wrapper:s}={...i(),...r.components};return s?n.jsx(s,{...r,children:n.jsx(l,{...r})}):l(r)}export{c as default,d as frontmatter};
