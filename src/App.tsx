9c1163ffc9dac11e6e31f621df0f471ff14eb1c0
Gigi <grdimitrov@gmail.com>
2025-10-09 20:09:33 +0200

    adding matomo tag

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Suspense, lazy } from "react";
 import { ThemeProvider } from "@/components/theme-provider";
@@ -26,6 +27,14 @@ const PageLoader = () => (
 );
 
 function App() {
+
+  React.useEffect(() => {
+   var _mtm = window._mtm = window._mtm || [];
+   _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+   var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+   g.async=true; g.src='https://cenite2-matomo.xxkpxb.easypanel.host/js/container_2OtD161c.js'; s.parentNode.insertBefore(g,s);
+  }, [])
+
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Router>
